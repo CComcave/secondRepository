6fc40208b4908b63a1687e2fdc88552831c8dd67
lex.neufeld <lex.neufeld@gmail.com>
2024-03-12 11:19:23 +0100

    non optimised code optimised

--- src/TestKlasse.java
+++ src/TestKlasse.java
@@ -2,7 +2,7 @@
 public class TestKlasse {
 
 	public static void main(String[] args) {
-		System.out.println("Daniel_Barsante");
+		System.out.println("Daniel is funny");
 		
 		// Es ist eine text zum push von Daniel Barsante
 
