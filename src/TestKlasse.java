fba6b28c58bf349c1049de209aab202fd045fa97
Daniel-Barsante <152161274+Daniel-Barsante@users.noreply.github.com>
2024-03-12 12:17:19 +0100

    Noch eine Konflikt test

--- src/TestKlasse.java
+++ src/TestKlasse.java
@@ -2,8 +2,8 @@ public class TestKlasse {
 
 	public static void main(String[] args) {
 
-		System.out.println("Daniel is funny");	
-		System.out.println("TestKlasse");
+		System.out.println("Daniel is not so funny");	
+		System.out.println("TestKlasse KK");
 		// Es ist eine text zum push von Daniel Barsante
 
 	}
