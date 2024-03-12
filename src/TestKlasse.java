a920619c06d17e06201f14017e499b0742a25fbe
Daniel-Barsante <152161274+Daniel-Barsante@users.noreply.github.com>
2024-03-12 11:16:37 +0100

    Test zum conflict

--- src/TestKlasse.java
+++ src/TestKlasse.java
@@ -2,7 +2,8 @@
 public class TestKlasse {
 
 	public static void main(String[] args) {
-		System.out.println("TestKlasse");
+		System.out.println("Daniel_Barsante");
+		
 		// Es ist eine text zum push von Daniel Barsante
 
 	}
