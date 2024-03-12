37398f2f4697b459a16e0ecc0e745ab2f80e253d
CComcave <mimika2@gmx.de>
2024-03-12 12:21:11 +0100

    Neue Testklasse Änderung

--- src/TestKlasse.java
+++ src/TestKlasse.java
@@ -3,8 +3,12 @@ public class TestKlasse {
 	public static void main(String[] args) {
 
 		System.out.println("Daniel is funny");	
-		System.out.println("TestKlasse");
+		System.out.println("Teste Klasse");
+		System.out.println("Daniel is not so funny");	
+		System.out.println("TestKlasse KK");
+		
 		// Es ist eine text zum push von Daniel Barsante
+		// Es ist eine text zum push von Monika
 
 	}
 
